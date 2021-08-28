06a93420ed7a6d13f3fb62d17433a4f10008b335
Stan <skukhniy@gmail.com>
2021-08-28 16:41:44 +0100

    added player factory and event listeners now work to display new moves

--- script.js
+++ script.js
@@ -5,18 +5,47 @@
 // register whos X and whose O
 // ['x','x','x','x','x','x','x','x','x']
 
+
+
+const Player = (name,move,turn) =>{
+    const getPlayerName = ()=>{
+        return name
+    }
+    const getMoveType = ()=>{
+        return move
+    }
+
+    let currentTurn = turn
+
+    const getTurn = ()=> {
+        return currentTurn
+    }
+    const nextTurn = ()=>{
+        if(currentTurn === true){
+            currentTurn = false
+        }else{
+            currentTurn = true
+        }
+    }
+    return{getPlayerName,getMoveType,getTurn,nextTurn}
+};
+
 const gameBoard = (()=>{
     const boardArray = ['','','','','','','','','']
+    //intialize players
+    const player1 = Player('player1','X',true)
+    const player2 = Player('player2','O',false)
 
-
-    return{boardArray}
+    return{boardArray,player1,player2}
 
 })();
 
 const displayController = (()=>{
     const boxSelectors = document.getElementsByClassName("box")
 
-    const render = (gameBoard.boardArray.forEach((move,index)=>{
+    const render = () => (gameBoard.boardArray.forEach((move,index)=>{
+            console.log("render check")
+            console.log(gameBoard.boardArray)
             boxSelectors[index].innerHTML = move
         }));
     
@@ -24,28 +53,26 @@ const displayController = (()=>{
 
 })();
 
-const Player = () =>{
-    
-    
-
-
-};
-
 const game = (() => {
-    let lastX = false
-
-
-
-    
     // NEED TO FIX 
-    Object.keys(displayController.boxSelectors).forEach((selector,index)=>{
-        selector.addEventListener('click',() =>{
-            if(lastX === False){
-                gameBoard.boardArray[index] = 'X'
+    Object.keys(displayController.boxSelectors).forEach((index)=>{
+        displayController.boxSelectors[index].addEventListener('click',() =>{
+            if(gameBoard.player1.getTurn() === true){
+                gameBoard.boardArray[index] = gameBoard.player1.getMoveType()
+                console.log("player 1 turn")
+                gameBoard.player1.nextTurn()
+                gameBoard.player2.nextTurn()
+            }else if(gameBoard.player2.getTurn() === true){
+                gameBoard.boardArray[index] = gameBoard.player2.getMoveType()
+                console.log("player 2 turn")
+                gameBoard.player1.nextTurn()
+                gameBoard.player2.nextTurn()
             }else{
-                gameBoard.boardArray[index] = 'O'
+                console.log(gameBoard.player1.getTurn())
+                console.log(gameBoard.player2.getTurn())
             }
-            displayController.render
+            displayController.render()
+            console.log(gameBoard.boardArray)
         });
     })
 })();
