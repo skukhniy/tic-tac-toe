5f8fe507f70b247bed043f5bb1d7bc241bdcb200
Stan <skukhniy@gmail.com>
2021-09-05 14:18:40 -0400

    added minimax algrothim, best move function, fixed 2player button

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// stop boxes from being clicked twice.
-// logic to see if you won the game/draw/loss
-// register whos X and whose O
-// ['x','x','x','x','x','x','x','x','x']
 
-
-
+//player factory thatll make a new player module whenever it is called.
 const Player = (name,move,turn) =>{
     const getPlayerName = ()=>{
         return name
@@ -12,7 +7,7 @@ const Player = (name,move,turn) =>{
     const getMoveType = ()=>{
         return move
     }
-
+    // this logic helps determine whose turn is it
     let currentTurn = turn
     const getTurn = ()=> {
         return currentTurn
@@ -27,16 +22,41 @@ const Player = (name,move,turn) =>{
     return{getPlayerName,getMoveType,getTurn,nextTurn}
 };
 
+// contains the board array, initalizes two players, and has the check winner function
 const gameBoard = (()=>{
-    const boardArray = ['','','','','','','','','']
+    const boardArray = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
     //intialize players
     const player1 = Player('player1','X',true)
     const player2 = Player('player2','O',false)
 
-    return{boardArray,player1,player2}
+    const checkWinner = (check=false)=>{
+        const winningCombos = [[0,1,2],[3,4,5],[6,7,8],
+                                [0,3,6],[1,4,7],[2,5,8],
+                                [0,4,8],[2,4,6]]
+        for (var i = 0; i< winningCombos.length; i++){
+            let winStringCheck = ''
+            winningCombos[i].forEach((box)=>{
+                winStringCheck += gameBoard.boardArray[box]
+            })
+            if(check){
+                console.log(gameBoard.boardArray)
+                console.log(winStringCheck)
+            }
+            if(winStringCheck === 'XXX' || winStringCheck === 'OOO'){
+                return(winStringCheck[0])
+            }else if(gameBoard.boardArray.includes(' ') === false){
+                //setTimeout(()=>{alert("It's a Tie")},1)
+                return('tie')
+            }
+        }
+        return(null)
+    }
+    return{boardArray,player1,player2,checkWinner}
 
+    
 })();
 
+// contains selectors from HTML
 const displayController = (()=>{
     const boxSelectors = document.getElementsByClassName("box")
 
@@ -46,7 +66,6 @@ const displayController = (()=>{
     const vsPlayer = document.getElementById("player2")
 
     const render = () => (gameBoard.boardArray.forEach((move,index)=>{
-            // console.log(gameBoard.boardArray)
             boxSelectors[index].innerHTML = move
         }));
     
@@ -54,85 +73,134 @@ const displayController = (()=>{
 
 })();
 
+// holds info for the CPU such as move type
 const CPU =(()=>{
-// set up functions that'll grab random moves
-//later on get functions that will always pick the optimal move.
     let openMoves = []
     const cpuMoveType = ()=>{return('O')}
-    const cpuMove = ()=>{
-        gameBoard.boardArray.forEach((box,index)=>{
-            if(box === ''){
-                openMoves.push(index)
-            }
-        })
-        let move = openMoves[Math.floor(Math.random() * openMoves.length)]
-        return(move)
-    }
-    return{cpuMove,cpuMoveType}
+    return{cpuMoveType}
 })();
 
-const game = (() => {
-    const checkWinner = ()=>{
-        const winningCombos = [[0,1,2],[3,4,5],[6,7,8],
-                                [0,3,6],[1,4,7],[2,5,8],
-                                [0,4,8],[2,4,6]]
-        for (var i = 0; i< winningCombos.length; i++){
-            let winStringCheck = ''
-            winningCombos[i].forEach((box)=>{
-                winStringCheck += gameBoard.boardArray[box]
-            })
-            console.log(winStringCheck)
-            if(winStringCheck === 'XXX' || winStringCheck === 'OOO'){
-                // COME BACK TO DECIDE WHAT TO DO ONCE A WINNER IS DECIDED (RESET GAMEBOARD?)
-                setTimeout(()=>{alert('WINNER!!!')},1)
-                break
+
+// contains both the minimax algrothim and a bestmove function to determine the best move for the cpu
+const ai = (()=>{
+    const bestMove = ()=>{
+        let bestScore = -Infinity;
+        let move;
+
+        gameBoard.boardArray.forEach((box,index)=>{
+        if(box === ' '){
+            gameBoard.boardArray[index] = CPU.cpuMoveType()
+            let score = minimax(gameBoard.boardArray,0,false);
+            gameBoard.boardArray[index] = ' ';
+            if (score > bestScore){
+                bestScore = score;
+                move = index
             }
-        }
+            }
+        })
+
+        gameBoard.boardArray[move] = CPU.cpuMoveType();
     }
+
+    let scores ={
+        'O':1,
+        'tie':0,
+        'X':-1
+        }
+
+    const minimax = (board,depth,maximizingPlayer)=>{
+        let result = gameBoard.checkWinner()
+        if (result !== null){
+            return scores[result];
+        }
+        
+        if(maximizingPlayer){
+            let bestScore = -Infinity
+            board.forEach((box,index)=>{
+                // is spot avalible?
+                if(box === ' '){
+                    board[index] = CPU.cpuMoveType()
+                    let score = minimax(board,depth+1,false)
+                    board[index] = ' ';
+                    bestScore = Math.max(score,bestScore);
+                }
+            }) 
+            return bestScore;
+        }else{
+            let bestScore = Infinity;
+            board.forEach((box,index)=>{
+                if(box === ' '){
+                    board[index] = gameBoard.player1.getMoveType()
+                    let score = minimax(board,depth+1,true)
+                    board[index] = ' ';
+                    bestScore = Math.min(score,bestScore)
+                }
+        })
+        return bestScore;
+    }
+}
+    return{bestMove}
+})();
+
+
+
+const game = (() => {
+    // checks to see if the game is vs the CPU or vs a real player
     let cpuCheck = true
     let playerCheck = false
     displayController.vsCPU.addEventListener('click',()=>{
-        if (gameBoard.boardArray === ['','','','','','','','','']){
+        if (gameBoard.boardArray.toString() === [" ", " ", " ", " ", " ", " ", " ", " ", " "].toString()){
             cpuCheck = true
             playerCheck = false
+            console.log(cpuCheck)
             }
     });
     displayController.vsPlayer.addEventListener('click',()=>{
-        if (gameBoard.boardArray === ['','','','','','','','','']){
+        if (gameBoard.boardArray.toString() === [" ", " ", " ", " ", " ", " ", " ", " ", " "].toString()){
         cpuCheck = false
         playerCheck = true
+        console.log(playerCheck)
         }
     });
+
+    // adjusts the gameboard array based on where player 1 clicked. also controls player2/cpu moves
     Object.keys(displayController.boxSelectors).forEach((index)=>{
         displayController.boxSelectors[index].addEventListener('click',() =>{
-            if(gameBoard.player1.getTurn() === true && gameBoard.boardArray[index] == ''){
+            if(gameBoard.player1.getTurn() === true && gameBoard.boardArray[index] == ' '){
                 gameBoard.boardArray[index] = gameBoard.player1.getMoveType()
-                //console.log("player 1 turn")
                 if(playerCheck === true){
                     gameBoard.player1.nextTurn()
                     gameBoard.player2.nextTurn()
                 }
-            }else if(gameBoard.player2.getTurn() === true && playerCheck === true && gameBoard.boardArray[index] == ''){
-                    gameBoard.boardArray[index] = gameBoard.player2.getMoveType()
-                    gameBoard.player2.nextTurn()
-                    gameBoard.player1.nextTurn()
-                    console.log('PLAYER 1 TURNED')
+            }else if(gameBoard.player2.getTurn() === true && playerCheck === true && gameBoard.boardArray[index] == ' '){
+                gameBoard.boardArray[index] = gameBoard.player2.getMoveType()
+                gameBoard.player2.nextTurn()
+                gameBoard.player1.nextTurn()
             }else{
-                console.log(gameBoard.player1.getTurn())
-                console.log(gameBoard.player2.getTurn())
+                console.log(null)
+                return(null)
+                
             }
             if(cpuCheck === true){
-                gameBoard.boardArray[CPU.cpuMove()] = CPU.cpuMoveType()
+                ai.bestMove()
             }
             displayController.render()
-            checkWinner()
+            let winner = gameBoard.checkWinner(check =true)
+            console.log(winner)
+            if(winner === 'X'|| winner==='O'){
+                setTimeout(()=>{alert('WINNER!!!')},1)
+            }else if (winner ==='tie'){
+                setTimeout(()=>{alert('tie')},2)
+            }
             console.log(gameBoard.boardArray)
         });
     })
 
     displayController.resetSelector.addEventListener('click',()=>{
-        gameBoard.boardArray = ['','','','','','','','','']
+        gameBoard.boardArray = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
         displayController.render()
     })
 
 })();
+
+
